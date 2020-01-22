fd01f5e6e1a2fa6a347722acbe0c4572c57f0cbf
John Thielman <john.thielman@gmail.com>
2020-01-21 20:14:54 -0600

    auth works

---
+++ src/components/shared/MyNavbar/MyNavbar.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import './MyNavbar.scss';
+
+class MyNavbar extends React.Component {
+  static propTypes = {
+    authed: PropTypes.bool,
+  }
+
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+  logMeOut = (e) => {
+    e.preventDefault();
+    firebase.auth().signOut();
+  }
+
+  render() {
+    const { authed } = this.props;
+    const buildNavbar = () => {
+      if (authed) {
+        return (
+          <ul className='navbar-nav ml-auto'>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/'>Home</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/words'>Words</Link>
+            </li>
+            <li className='nav-item'>
+              <Link className='nav-link' to='/words/new'>New</Link>
+            </li>
+            <li className='nav-item'>
+              <button className='nav-link btn btn-outline-dark' onClick={this.logMeOut}>Log out</button>
+            </li>
+          </ul>
+        );
+      }
+      return (
+        <ul className='navbar-nav ml-auto'>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/'>Home</Link>
+          </li>
+          <li>
+            <Link className='nav-link' to='/words'>Words</Link>
+          </li>
+          <li>
+            <button className='nav-link btn btn-light' onClick={this.loginClickEvent}>Log in with Google</button>
+          </li>
+        </ul>
+      )
+    };
+    return (
+      <div className="MyNavbar">
+        <nav className="navbar navbar-expand navbar-light bg-light">
+          <Link className="nav-brand" to="/">Anglish Wordbook</Link>
+          { buildNavbar() }
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default MyNavbar;

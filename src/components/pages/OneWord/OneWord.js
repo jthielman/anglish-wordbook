7bb986f902279735a3362807f71aa2423a4b2cde
John Thielman <john.thielman@gmail.com>
2020-01-22 21:07:56 -0600

    fixed logged-out routing bugs

--- src/components/pages/OneWord/OneWord.js
+++ src/components/pages/OneWord/OneWord.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+// import authData from '../../../helpers/data/authData';
 
 import './OneWord.scss';
 
 class OneWord extends React.Component {
   render() {
     const wordId = '12345';
+    const user = firebase.auth().currentUser;
     return (
       <div>
         <h1>One Word</h1>
-        <Link className='btn' to={`/words/${wordId}/adight`}>edit</Link>
+        { user ? <Link className='btn' to={`/words/${wordId}/adight`}>adight</Link> : <div>you logged out</div> }
       </div>
     );
   }
---
+++ src/helpers/data/authData.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+const getUid = () => firebase.auth().currentUser.uid;
+const getUser = () => firebase.auth().currentUser;
+
+export default { getUid, getUser };
